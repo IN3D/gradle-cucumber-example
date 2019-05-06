5d5bf40aa8c7f95d69b6000ac7d0ea61ce6805ad
Eric Hopkins <eric.on.tech@gmail.com>
2019-05-06 18:13:36 -0400

    Add toString overrides for Attribute enum
    Also adds the missing annotations to the Learning enum.

--- src/main/java/io/mekmanager/example/Attribute.java
+++ src/main/java/io/mekmanager/example/Attribute.java
@@ -1,12 +1,52 @@
 package io.mekmanager.example;
 
 public enum Attribute {
-    STR,
-    BOD,
-    RFL,
-    DEX,
-    INT,
-    WIL,
-    CHA,
-    EDG
+    STR {
+        @Override
+        public String toString() {
+            return "Strength";
+        }
+    },
+    BOD {
+        @Override
+        public String toString() {
+            return "Body";
+        }
+    },
+    RFL {
+        @Override
+        public String toString() {
+            return "Reflexes";
+        }
+    },
+    DEX {
+        @Override
+        public String toString() {
+            return "Dexterity";
+        }
+    },
+    INT {
+        @Override
+        public String toString() {
+            return "Intelligence";
+        }
+    },
+    WIL {
+        @Override
+        public String toString() {
+            return "Willpower";
+        }
+    },
+    CHA {
+        @Override
+        public String toString() {
+            return "Charisma";
+        }
+    },
+    EDG {
+        @Override
+        public String toString() {
+            return "Edge";
+        }
+    }
 }
--- src/main/java/io/mekmanager/example/Learning.java
+++ src/main/java/io/mekmanager/example/Learning.java
@@ -2,16 +2,19 @@ package io.mekmanager.example;
 
 public enum Learning {
     FAST {
+        @Override
         public String toString() {
             return "Fast";
         }
     },
     STANDARD {
+        @Override
         public String toString() {
             return "Standard";
         }
     },
     SLOW {
+        @Override
         public String toString() {
             return "Slow";
         }
